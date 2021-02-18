f939f7f5412666a34fab11734851dc54d52bb42b
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2021-02-18 15:34:18 +0300

    Delete index.js

--- public_html/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const http = require('http')
-http.Server((req, res) =>  {
-    if (req.url === '/login') return res.end('190522');
-    if (req.url === '/sample') return res.end('text/plain');
-    res.end(String(fs.readFileSync(__filename)));
-})
-.listen(4321);
