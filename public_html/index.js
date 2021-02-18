20c45d17544e2e2f8cb0c8be1db6acca75fc01b4
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2021-02-18 14:46:09 +0300

    Update index.js

--- public_html/index.js
+++ public_html/index.js
@@ -1,8 +1,8 @@
-f(x) => (return x*x);
 const fs = require('fs');
 const http = require('http')
 http.Server((req, res) =>  {
-    if (req.url === '/login' return '190522';
-    if (req.url === '/sample' return 'text/plain';
-    res.end(String(fs.readFileSync(__filename))))
+    if (req.url === '/login') return res.end('190522');
+    if (req.url === '/sample') return res.end('text/plain');
+    res.end(String(fs.readFileSync(__filename)));
+})
 .listen(4321);
